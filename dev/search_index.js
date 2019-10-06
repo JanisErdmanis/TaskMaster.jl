5d549224881b948cf879e8a1f048419b4eb6484b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-06 19:29:59 +0000

    build based on 5517981

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Introduction","title":"Introduction","text":"EditURL = \"@__REPO_ROOT_URL__/\"","category":"page"},{"location":"#Introduction-1","page":"Introduction","title":"Introduction","text":"","category":"section"},{"location":"#","page":"Introduction","title":"Introduction","text":"TaskMaster is a tool for modeling feedback loops with paralelism. The design allows to easally swap executor Master which allows one to configure the system as one wishes. It also allows to swap Learner as one wishes for a particular problem. Also one can always reproduce the state of the learner with the design if learner itself is pure. See examples folder for know.","category":"page"}]
+}
